bbe1de10728dfbb626b0ef13ba8e86937ca9e9bc
Leif <leif.k.boutin@gmail.com>
2022-03-08 18:25:16 +0100

    backend done? post, update & get seem to work, go is .. different :/

--- backend-bookeeper/go.mod
+++ backend-bookeeper/go.mod
@@ -5,6 +5,7 @@ go 1.17
 require rsc.io/quote v1.5.2
 
 require (
+	github.com/gin-contrib/sessions v0.0.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-bongo/bongo v0.10.4 // indirect
@@ -14,6 +15,10 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- backend-bookeeper/main.go
+++ backend-bookeeper/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -17,24 +19,85 @@ import (
 // var port string = "8080"
 
 
+var router *gin.Engine
 var ctx context.Context
-// var cancel context.CancelFunc
 var collectionAccounts *mongo.Collection
+var collectionBooks *mongo.Collection
 type accountBody struct {
 	Account string `json:"account"`
 	Password string `json:"password"`
 	Admin bool  `json:"admin"`
 }
+type accountLogin struct {
+	Account string `json:"account"`
+	Password string `json:"password"`
+}
+type bookshelfBody struct {
+	Owner string `bson:"owner" json:"owner"`
+	Book string `bson:"book" json:"book"`
 
-// type bookshelf struct {
-// 	Id		string
-// 	Account string
-// 	Password string
-// 	Admin bool
-// }
+}
+type bookshelf struct {
+	Owner string `bson:"owner" json:"owner"`
+	Books []string `bson:"books" json:"books"`
+}
+
+func getMyBookshelves(c * gin.Context){
+	var requestBody bookshelfBody
+	if err := c.BindJSON(&requestBody); err != nil {
+		fmt.Print(err)
+	}
+	var bookshelfObject bookshelf
+	if err := collectionBooks.FindOne(ctx, bson.M{"owner": requestBody.Owner}).Decode(&bookshelfObject); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(bookshelfObject)
+	c.IndentedJSON(http.StatusOK, bookshelfObject)
+
+}
+func addBookToBookeeper(c * gin.Context){
+	var requestBody bookshelfBody
+
+	if err := c.BindJSON(&requestBody); err != nil {
+		fmt.Println(err)
+		
+		return
+	}
+	var bookshelfObject bookshelf
+	if err := collectionBooks.FindOne(ctx, bson.M{"owner": requestBody.Owner}).Decode(&bookshelfObject); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(bookshelfObject.Books)
+	newCollection := append(bookshelfObject.Books, requestBody.Book)
+	fmt.Println(newCollection)
+	
+	result, err := collectionBooks.ReplaceOne(ctx, bson.M{"owner": requestBody.Owner}, bson.M{"owner": requestBody.Owner, "books":newCollection})
+	if err != nil {
+	fmt.Println(err)
+		c.IndentedJSON(http.StatusForbidden, gin.H{"error": err})
+		return
+	}
+	fmt.Println(result)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Books added to the collection."})
+	
+	// if err != nil {
+	
+	// 	c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Bookeshelf already exists.", "error": err.Error(), "account": requestBody.Account})
+	// 	return
+	// }
+	// var account accountLogin
+	// if err := collectionAccounts.FindOne(ctx, bson.M{"account": requestBody.Owner}).Decode(&account); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+}
 func hashingPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
+}
+func checkPassword(password, hash string) (bool) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 func createBookShelf(c * gin.Context){
 	var requestBody accountBody
@@ -46,20 +109,36 @@ func createBookShelf(c * gin.Context){
 	
 	hash, _ := hashingPassword(requestBody.Password)
 	requestBody.Password = hash
-	newBookShelf := accountBody{
+	newAccount := accountBody{
 		Account: requestBody.Account,
 		Password: hash,
 		Admin: false,
 	}
-	result, err := collectionAccounts.InsertOne(ctx, newBookShelf)
+
+	result, err := collectionAccounts.InsertOne(ctx, newAccount)
 	if err != nil {
-		println(err)
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
+	
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Bookeshelf already exists.", "error": err.Error(), "account": requestBody.Account})
 		return
 	}
+	newBookshelf := bookshelf{
+
+		Owner: requestBody.Account,
+	
+	}
+	resultBookshelf, err := collectionBooks.InsertOne(ctx, newBookshelf)
+	if err != nil {
+		c.IndentedJSON(http.StatusForbidden, gin.H{"message": "bookshelf not created"})
+		return
+	}
+	println(resultBookshelf)
 	c.IndentedJSON(http.StatusOK, result)
+	
+
+	
+
 }
-func getAllBookshelves(c *gin.Context){
+func getAllAccounts(c *gin.Context){
 	cur, currErr := collectionAccounts.Find(ctx, bson.M{})
 	if currErr != nil {
 		panic(currErr)
@@ -74,11 +153,12 @@ func getAllBookshelves(c *gin.Context){
 		}
 		accounts = append(accounts, account)
 	}
+	
 	c.IndentedJSON(http.StatusOK, accounts)
 	
 }
 func loginToBookeeper(c* gin.Context){
-	var requestBody accountBody
+	var requestBody accountLogin
 
 	if err := c.BindJSON(&requestBody); err != nil {
 		println(err)
@@ -86,21 +166,33 @@ func loginToBookeeper(c* gin.Context){
 	}
 
 
-	var account bson.M
+	var account accountLogin
 	if err := collectionAccounts.FindOne(ctx, bson.M{"account": requestBody.Account}).Decode(&account); err != nil {
 		log.Fatal(err)
 	}
-	if account["password"] != requestBody.Password {
-		
-		c.IndentedJSON(http.StatusForbidden, gin.H{"message": "password is incorrect."})
+	// var password string
+
 	
+	result := checkPassword(requestBody.Password, account.Password )
+	if !result{
+		c.IndentedJSON(http.StatusForbidden, gin.H{"message": "password is incorrect."})
 		return 
 	}
-	c.IndentedJSON(http.StatusAccepted, account)
+	
+	// session := sessions.Default(c)
+	// session.Set("bookeeperCookie", "jesuisLEcookie?!")
+	// session.Save()
+	c.IndentedJSON(http.StatusOK, gin.H{"account":account.Account, "token": "#TuesBienLogaMonApplicaTionGoJefEraideLasecuPlusT4rd!"})
 }
 
 func main(){
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://Leif:Kleenex31001827!@cluster0.0mzkd.mongodb.net/Bookshelf?retryWrites=true&w=majority"))
+	err := godotenv.Load(".env")
+	if err != nil {	
+		fmt.Println("Error loading .env")
+	}else{
+		fmt.Println(".env loaded")
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DATABASE_URL")))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -111,16 +203,34 @@ func main(){
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
-	collectionAccounts = client.Database("Bookshelf").Collection("accounts")
+	
 
-	router := gin.Default()
-	router.GET("/bookshelves", getAllBookshelves)
+
+	collectionAccounts = client.Database("Bookshelf").Collection("accounts")
+	collectionBooks = client.Database("Bookshelf").Collection("Books")
+	indexOptions := options.Index().SetUnique(true)
+	indexKeys := bsonx.MDoc{"account": bsonx.Int32(-1),}
+	accountIndexModel := mongo.IndexModel{Options: indexOptions, Keys: indexKeys}
+	
+
+	indexName, err := collectionAccounts.Indexes().CreateOne(ctx, accountIndexModel)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	println(indexName)
+if err != nil {
+	log.Fatalf("something went wrong: %+v", err)
+}
+
+	router = gin.Default()
+	// cookie_store := cookie.NewStore([]byte("bookEEperCookie"))
+  // router.Use(sessions.Sessions("session",cookie_store))
+	router.GET("/bookshelves", getAllAccounts)
+	router.POST("/my-books", getMyBookshelves)
 	router.POST("/login", loginToBookeeper)
 	router.POST("/register", createBookShelf)
+	router.POST("/add-book", addBookToBookeeper)
 	router.Run()
 	fmt.Println("DB connected & Server Listening on port 8080")
 
